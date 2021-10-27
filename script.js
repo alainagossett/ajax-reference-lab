0db03354ed84f6703b37d60e0b980aa3eba1a024
Alaina <alaina.gossett@gmail.com>
2021-10-26 18:10:16 -0600

    wip: adds js boilerplate

---
+++ script.js
@@ -0,0 +1,23 @@
+// CONSTANTS
+const BASE_URL = '';
+const API_KEY = '';
+
+// STATE VARIABLES
+let apiData;
+
+// CACHED ELEMENT REFERENCES
+const $main = $('main');
+
+
+// EVENT LISTENERS
+
+
+// FUNCTIONS
+function getData() {
+    //get API data and assign it to our apiData variable
+
+}
+
+function render() {
+    //transfer API data to the DOM
+}
